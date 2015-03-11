2a841f4e9a6b2333d05b35113673846d68402260
aaron-hardin <teslasprotege@gmail.com>
2015-03-10 23:07:27 -0500

    init
    currently just sample content

---
+++ VisualStudio.Go/VisualStudio.GoFormat.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace VisualStudio.Go
+{
+    #region Format definition
+    /// <summary>
+    /// Defines an editor format for the VisualStudio.Go type that has a purple background
+    /// and is underlined.
+    /// </summary>
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = "VisualStudio.Go")]
+    [Name("VisualStudio.Go")]
+    [UserVisible(true)] //this should be visible to the end user
+    [Order(Before = Priority.Default)] //set the priority to be after the default classifiers
+    internal sealed class VisualStudio.GoFormat : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Defines the visual format for the "VisualStudio.Go" classification type
+        /// </summary>
+        public VisualStudio.GoFormat()
+        {
+            this.DisplayName = "VisualStudio.Go"; //human readable version of the name
+            this.BackgroundColor = Colors.BlueViolet;
+            this.TextDecorations = System.Windows.TextDecorations.Underline;
+        }
+    }
+    #endregion //Format definition
+}
---
+++ VisualStudio.Go/VisualStudio.GoType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace VisualStudio.Go
+{
+    internal static class VisualStudio.GoClassificationDefinition
+    {
+        /// <summary>
+        /// Defines the "VisualStudio.Go" classification type.
+        /// </summary>
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name("VisualStudio.Go")]
+        internal static ClassificationTypeDefinition VisualStudio.GoType = null;
+    }
+}
